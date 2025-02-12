858d74108d9ac593b35254594e935448e0891b4f
delmastv <delmastv@sheridancollege.ca>
2025-02-12 18:58:26 -0500

    In the player script I added the slider component so that the walking speed of the player gets altered with the slider. made the slider move with whole numbers in the inspector, there still seem to be some further tweaking that needs to be done to make it run the way I want, the slider still moves around when the player moves which is something that needs to be fixed

--- Assets/VIENNAS SCRIPTS FOR PROJECT/playermoving.cs
+++ Assets/VIENNAS SCRIPTS FOR PROJECT/playermoving.cs
@@ -1,32 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playermoving : MonoBehaviour
 {
 
-        public float chaspeed = 5f;
-        public Rigidbody2D body;
-        public Animator anim;
+    public float chaspeed = 5f;
+    public Rigidbody2D body;
+    public Animator anim;
+    public Slider speedSlider;
 
-        private Vector2 direction;
+    private Vector2 direction;
 
-        void Update()
+    void Start()
+    {
+        if (speedSlider != null)
         {
-            direction.x = Input.GetAxisRaw("Horizontal");
-            direction.y = Input.GetAxisRaw("Vertical");
+            speedSlider.value = chaspeed;
 
-            if (direction.x != 0 || direction.y != 0)
-            {
-                anim.SetFloat("Horizontal", direction.x);
-                anim.SetFloat("Vertical", direction.y);
-            }
-
-            anim.SetFloat("Speed", direction.sqrMagnitude);
-        }
-
-        void FixedUpdate()
-        {
-            body.MovePosition(body.position + direction * chaspeed * Time.fixedDeltaTime);
         }
     }
+
+    void Update()
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+
+        if (direction.x != 0 || direction.y != 0)
+        {
+            anim.SetFloat("Horizontal", direction.x);
+            anim.SetFloat("Vertical", direction.y);
+        }
+
+        anim.SetFloat("Speed", direction.sqrMagnitude);
+
+        if (speedSlider != null)
+        {
+            chaspeed = speedSlider.value;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        body.MovePosition(body.position + direction * chaspeed * Time.fixedDeltaTime);
+    }
+
+}
