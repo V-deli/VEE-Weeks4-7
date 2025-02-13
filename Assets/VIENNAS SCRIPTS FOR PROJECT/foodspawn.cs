1d70dacfc3ecf779a6f4863cf027d76826b3e109
delmastv <delmastv@sheridancollege.ca>
2025-02-12 21:38:34 -0500

    I got the food sprites working at the time I want and with the random sprite from array I wanted too, just got to move them where I want which is at the top of the screen not the bottom

--- Assets/VIENNAS SCRIPTS FOR PROJECT/foodspawn.cs
+++ Assets/VIENNAS SCRIPTS FOR PROJECT/foodspawn.cs
@@ -5,18 +5,30 @@ using UnityEngine;
 public class foodspawn : MonoBehaviour
 {
     public GameObject[] foodPrefabs;
-    public float spawnTime = 2f;
-    public float despawnTime = 5f;
-    public Vector2 spawnOffset = new Vector2(3f, 0f);
+    public float spawnTime = 20.5f;
+    public float despawnTime = 20f;
+    public Vector2 spawnOffset = new Vector2(3f, 3f);
+
+    private float timer = 0f;
 
 
     private void Start()
     {
-        
+        SpawnFood();
     }
 
-    
-    void Update()
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= spawnTime)
+        {
+            SpawnFood();
+            timer = 0f;
+        }
+    }
+    void SpawnFood()
     {
         for (int i = 0; i < 10; i++)
         {
@@ -27,8 +39,9 @@ public class foodspawn : MonoBehaviour
                 (col - 2) * spawnOffset.x,
                 (row - 2) * spawnOffset.y
                 );
-            GameObject food = Instantiate(foodPrefabs[Random.Range(0, foodPrefabs.Length)], spawnPosition, Quaternion.identity );
-            Destroy( food, despawnTime);
+            GameObject food = Instantiate(foodPrefabs[Random.Range(0, foodPrefabs.Length)], spawnPosition, Quaternion.identity);
+            Destroy(food, despawnTime);
+        }
         }
     }
-}
+
